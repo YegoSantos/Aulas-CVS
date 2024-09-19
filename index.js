dc6dab16235e09e32c5fd144dd22c6c273b8d475
Yego Santos <yegosantoss12@gmail.com>
2024-09-19 12:42:17 -0300

    Primeiro commit: adicionando projeto ao repositório

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Olá Mundo")
+
+var meu_primeiro_arrays = ["Yan", "Mayrla", "Yego"]
+console.log(meu_primeiro_arrays.length)
+// console.log(meu_primeiro_arrays[1])
+// console.log(meu_primeiro_arrays[2])
+dois_primeiros_alimentos = meu_primeiro_arrays.slice(1,3)
+console.log(dois_primeiros_alimentos)
